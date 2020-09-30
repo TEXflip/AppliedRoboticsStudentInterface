29a0d38fce3615598a2dff8d51f2b5e154803081
Ubuntu <ubuntu@ubuntu1604.linuxvmimages.local>
2020-09-30 16:23:07 -0400

    generic image Listener implementation

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -3,8 +3,11 @@
 
 #include <stdexcept>
 #include <sstream>
+#include <experimental/filesystem>
+
 namespace student
 {
+  const std::string RAW_IMAGE("/image/raw");
 
   void loadImage(cv::Mat &img_out, const std::string &config_folder)
   {
@@ -13,10 +16,42 @@ namespace student
 
   void genericImageListener(const cv::Mat &img_in, std::string topic, const std::string &config_folder)
   {
-    std::string info(topic + "\t" + config_folder);
-    cv::imshow("win1", img_in);
-    cv::waitKey(0);
-    throw std::logic_error("STUDENT FUNCTION - IMAGE LISTENER - NOT CORRECTLY IMPLEMENTED" + info);
+    static bool folderCreated = false;
+    static std::string save_path;
+    static int id = 0;
+
+    if (!folderCreated)
+    {
+      bool exist = true;
+      int i;
+      for (i = 0; exist && i < 1000; i++)
+      {
+        save_path = config_folder + "/saved_images" + std::to_string(i) + "/";
+        exist = std::experimental::filesystem::exists(save_path);
+      }
+      
+      if (i > 999 || !std::experimental::filesystem::create_directories(save_path))
+        throw std::logic_error("NO EMPTY FOLDER AVAILABLE");
+      
+      folderCreated = true;
+    }
+
+    cv::imshow(topic, img_in);
+    char c = cv::waitKey(30);
+
+    switch (c)
+    {
+    case 's':
+    {
+      std::string img_file(save_path + "img" + std::to_string(id++) + ".jpg");
+      cv::imwrite(img_file, img_in);
+
+      std::cout << "Saved image: " << img_file << std::endl;
+    }
+    break;
+    default:
+      break;
+    }
   }
 
   bool extrinsicCalib(const cv::Mat &img_in, std::vector<cv::Point3f> object_points, const cv::Mat &camera_matrix, cv::Mat &rvec, cv::Mat &tvec, const std::string &config_folder)
